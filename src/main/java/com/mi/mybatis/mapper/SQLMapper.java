e610811e62b0a70dafb88ed1f9ad0451b21d55c0
chengmengd <chengmengd@isoftstone.com>
2025-03-21 10:28:24 +0800

    特殊SQL执行

---
+++ src/main/java/com/mi/mybatis/mapper/SQLMapper.java
@@ -0,0 +1,36 @@
+package com.mi.mybatis.mapper;
+
+import com.mi.mybatis.entity.Employee;
+import com.mi.mybatis.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SQLMapper {
+
+    /**
+     * 根据用户名模糊查询用户信息
+     * @return
+     */
+    List<User> getUserByName(@Param("username") String username);
+
+    /**
+     * 批量删除用户信息
+     * @return
+     */
+    int batchDelete(@Param("ids") String ids);
+
+    /**
+     * 查询用户表中数据
+     * @param tableName
+     * @return
+     */
+    List<User> getUserByTableName(@Param("tableName") String tableName);
+
+    /**
+     * 添加用户信息
+     * @param user
+     * @return
+     */
+    int insertUser(User user);
+}
