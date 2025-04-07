dac23cee7416d5a35e2b07fa8bffb1c0986ecf0c
chengmengd <chengmengd@isoftstone.com>
2025-04-07 09:37:07 +0800

    springmvc-view-controller标签实现页面跳转

--- src/main/java/com/mi/springmvc/controller/IndexController.java
+++ src/main/java/com/mi/springmvc/controller/IndexController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String toIndex() {
-        return "index";
-    }
+    // @RequestMapping("/")
+    // public String toIndex() {
+    //     return "index";
+    // }
 
     @RequestMapping("/param")
-    public String param() {
+    public String toParam() {
         return "param";
+    }
+
+    @RequestMapping("/view")
+    public String toView() {
+        return "view";
+    }
+
+    @RequestMapping("/error")
+    public String toError() {
+        return "error";
     }
 }
