12ee8c54bfd6cc3757da7a2d9bfae5ae9e947ba6
chengmengd <chengmengd@isoftstone.com>
2026-08-22 19:06:57 +0800

    修改注释信息

--- src/test/unit/mybatis/TestCache.java
+++ src/test/unit/mybatis/TestCache.java
@@ -47,7 +47,7 @@ public class TestCache {
     /**
      * 二级缓存是SqlSessionFactory级别，通过同一个SqlSessionFactory创建的SqlSession查询的结果会被缓存，此后若再次执行相同的查询语句，结果就会从缓存中获取
      * 二级缓存开启的条件：
-     *      1、在核心配置文件中，设置全局配置属性cacheEnable="true"，默认为true，不需要设置
+     *      1、在核心配置文件中，设置全局配置属性cacheEnable="true"，开启二级缓存
      *      2、在映射文件中设置标签<cache />
      *      3、二级缓存必须在SqlSession关闭或提交之后有效
      *      4、查询的数据所转换的实体类类型必须实现序列化接口
@@ -63,6 +63,7 @@ public class TestCache {
             CacheMapper mapper = sqlSession.getMapper(CacheMapper.class);
             Emp emp = mapper.getEmpById(5L);
             System.out.println(emp);
+            // 关闭SqlSession
             sqlSession.close();
             System.out.println("==============================================================");
             SqlSession sqlSession2 = sqlSessionFactory.openSession(true);
