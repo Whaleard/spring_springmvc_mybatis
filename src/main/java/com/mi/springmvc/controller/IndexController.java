99d413ef03a6f1025d359c80bcbd6b0bb24346b1
chengmengd <chengmengd@isoftstone.com>
2026-08-24 16:18:52 +0800

    删除无效代码

--- src/main/java/com/mi/springmvc/controller/IndexController.java
+++ src/main/java/com/mi/springmvc/controller/IndexController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    // @RequestMapping("/")
-    // public String toIndex() {
-    //     return "index";
-    // }
-
     @RequestMapping("/param")
     public String toParam() {
         return "param";
