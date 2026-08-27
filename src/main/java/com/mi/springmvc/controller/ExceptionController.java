bd9baa039c5762ee91ac47939b4db557ed54da53
chengmengd <chengmengd@isoftstone.com>
2026-08-27 22:23:45 +0800

    SpringMVC异常处理器详解

--- src/main/java/com/mi/springmvc/controller/ExceptionController.java
+++ src/main/java/com/mi/springmvc/controller/ExceptionController.java
@@ -4,9 +4,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * @ControllerAdvice将当前类标识为异常处理的组件
+ */
 @ControllerAdvice
 public class ExceptionController {
 
+    /**
+     * @ExceptionHandler用于设置所标识方法处理的异常
+     *
+     * @param e 表示当前请求处理中出现的异常
+     * @param model
+     * @return
+     */
     @ExceptionHandler(value = {ArithmeticException.class, NullPointerException.class})
     public String testException(Exception e, Model model) {
         model.addAttribute("ex", e);
