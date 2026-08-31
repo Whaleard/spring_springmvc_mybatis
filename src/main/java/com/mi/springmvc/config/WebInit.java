5b200be5532504b0dddc76039bc942417eda4436
chengmengd <chengmengd@isoftstone.com>
2026-09-01 01:05:45 +0800

    Java Web应用初始化类

--- src/main/java/com/mi/springmvc/config/WebInit.java
+++ src/main/java/com/mi/springmvc/config/WebInit.java
@@ -8,6 +8,10 @@ import javax.servlet.Filter;
 
 /**
  * web工程的初始化类，用来代替web.xml
+ *
+ * 在Servlet3.0环境中，容器会在类路径下查找实现javax.servlet.ServletContainerInitializer接口的类，
+ * 这个类反过来又会查找实现WebApplicationInitializer的类并将配置的任务交给它们来完成。Spring3.2引入了一个便利的WebApplicationInitializer基础实现，名为AbstractAnnotationConfigDispatcherServletInitializer。
+ * 当一个类扩展了AbstractAnnotationConfigDispatcherServletInitializer并将其部署到Servlet3.0容器的时候，容器会自动发现它，并用它来配置Servlet上下文。
  */
 public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
